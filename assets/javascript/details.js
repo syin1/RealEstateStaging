30b4a6096158eb1f887d5e8a2361c5582358f96b
syin <shaomingyinut@gmail.com>
2018-07-29 14:27:47 -0400

    loading 4 images per detail page

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -14,23 +14,23 @@ $(document).ready(function() {
   var imageno = sessionStorage.getItem('image-no');
 
   $('#detailimage1').append(
-    "<img class='d-block w-100' src='assets/images/details/'" +
+    "<img class='d-block w-100' src='assets/images/details/" +
       imageno +
-      "'/1.jpg' height='500' width='500' alt='First slide'>"
+      "/1.jpg' height='500' width='500' alt='First slide'>"
   );
   $('#detailimage2').append(
-    "<img class='d-block w-100' src='assets/images/details/'" +
+    "<img class='d-block w-100' src='assets/images/details/" +
       imageno +
-      "'/2.jpg' height='500' width='500' alt='First slide'>"
+      "/2.jpg' height='500' width='500' alt='First slide'>"
   );
   $('#detailimage3').append(
-    "<img class='d-block w-100' src='assets/images/details/'" +
+    "<img class='d-block w-100' src='assets/images/details/" +
       imageno +
-      "'/3.jpg' height='500' width='500' alt='First slide'>"
+      "/3.jpg' height='500' width='500' alt='First slide'>"
   );
   $('#detailimage4').append(
-    "<img class='d-block w-100' src='assets/images/details/'" +
+    "<img class='d-block w-100' src='assets/images/details/" +
       imageno +
-      "'/4.jpg' height='500' width='500' alt='First slide'>"
+      "/4.jpg' height='500' width='500' alt='First slide'>"
   );
 });
