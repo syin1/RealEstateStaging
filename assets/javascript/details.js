764fe2a020f2ffb19a6f1e989644742688b73ad3
syin <shaomingyinut@gmail.com>
2018-07-29 14:25:56 -0400

    loading 4 images per detail page

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -1,1 +1,36 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  // sessionStorage.getItem('address');
+  // sessionStorage.getItem('baths');
+  // sessionStorage.getItem('beds');
+  // sessionStorage.getItem('description');
+  // sessionStorage.getItem('postalcode');
+  // sessionStorage.getItem('price');
+  // sessionStorage.getItem('size');
+  // sessionStorage.getItem('type');
+  // sessionStorage.getItem('utilities');
+
+  // sessionStorage.getItem('data-key');
+
+  var imageno = sessionStorage.getItem('image-no');
+
+  $('#detailimage1').append(
+    "<img class='d-block w-100' src='assets/images/details/'" +
+      imageno +
+      "'/1.jpg' height='500' width='500' alt='First slide'>"
+  );
+  $('#detailimage2').append(
+    "<img class='d-block w-100' src='assets/images/details/'" +
+      imageno +
+      "'/2.jpg' height='500' width='500' alt='First slide'>"
+  );
+  $('#detailimage3').append(
+    "<img class='d-block w-100' src='assets/images/details/'" +
+      imageno +
+      "'/3.jpg' height='500' width='500' alt='First slide'>"
+  );
+  $('#detailimage4').append(
+    "<img class='d-block w-100' src='assets/images/details/'" +
+      imageno +
+      "'/4.jpg' height='500' width='500' alt='First slide'>"
+  );
+});
