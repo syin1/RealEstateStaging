9241fc979a9c41bbf3b5743e37bd98d9590b090a
syin <shaomingyinut@gmail.com>
2018-07-29 20:17:26 -0400

    testing details authentication

--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyDYI4Fjm_RWQadwHH7KdTJGSErId9aUJKY',
+    authDomain: 'real-estate-app-9e4e6.firebaseapp.com',
+    databaseURL: 'https://real-estate-app-9e4e6.firebaseio.com',
+    projectId: 'real-estate-app-9e4e6',
+    storageBucket: 'real-estate-app-9e4e6.appspot.com',
+    messagingSenderId: '1087918608727'
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
   $('#edit').hide();
   $('#delete').hide();
 
