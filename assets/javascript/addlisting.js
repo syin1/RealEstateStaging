0451b0039ad0fbde79fae0d848cb6bc8818f658e
syin <shaomingyinut@gmail.com>
2018-07-31 18:47:29 -0400

    testing validation

--- assets/javascript/addlisting.js
+++ assets/javascript/addlisting.js
@@ -12,81 +12,84 @@ $(document).ready(function() {
 
   var database = firebase.database();
 
-  $('#addlistingform')
-    .validator()
-    .on('click', '#submit', function(event) {
-      // event.preventDefault(); // this step is necessary to trigger the default validation
-      if (e.isDefaultPrevented()) {
-        // handle the invalid form...
-      } else {
-        var user = firebase.auth().currentUser;
-        var name, email, photoUrl, uid, emailVerified;
+  $('#submit').on('click', function(event) {
+    // event.preventDefault(); // this step is necessary to trigger the default validation
 
-        if (user != null) {
-          name = user.displayName;
-          email = user.email;
-          photoUrl = user.photoURL;
-          emailVerified = user.emailVerified;
-          uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-          // this value to authenticate with your backend server, if
-          // you have one. Use User.getToken() instead.
+    $('#addlistingform')
+      .validator()
+      .on('submit', function(e) {
+        if (e.isDefaultPrevented()) {
+          // handle the invalid form...
+        } else {
+          var user = firebase.auth().currentUser;
+          var name, email, photoUrl, uid, emailVerified;
+
+          if (user != null) {
+            name = user.displayName;
+            email = user.email;
+            photoUrl = user.photoURL;
+            emailVerified = user.emailVerified;
+            uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+            // this value to authenticate with your backend server, if
+            // you have one. Use User.getToken() instead.
+          }
+
+          var address = $('#address')
+            .val()
+            .trim();
+
+          var postalcode = $('#postalcode')
+            .val()
+            .trim();
+
+          var price = $('#price')
+            .val()
+            .trim();
+
+          var beds = $('#beds')
+            .val()
+            .trim();
+
+          var baths = $('#baths')
+            .val()
+            .trim();
+
+          var description = $('#description')
+            .val()
+            .trim();
+
+          var type = $('#type')
+            .val()
+            .trim();
+
+          var utilities = $('#utilities')
+            .val()
+            .trim();
+
+          var size = $('#size')
+            .val()
+            .trim();
+
+          database.ref().push({
+            address: address,
+            postalcode: postalcode,
+            price: price,
+            beds: beds,
+            baths: baths,
+            description: description,
+            type: type,
+            utilities: utilities,
+            size: size,
+            name: name,
+            email: email,
+            uid: uid,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
+          });
+
+          window.location.href = 'index.html';
         }
-
-        var address = $('#address')
-          .val()
-          .trim();
-
-        var postalcode = $('#postalcode')
-          .val()
-          .trim();
-
-        var price = $('#price')
-          .val()
-          .trim();
-
-        var beds = $('#beds')
-          .val()
-          .trim();
-
-        var baths = $('#baths')
-          .val()
-          .trim();
-
-        var description = $('#description')
-          .val()
-          .trim();
-
-        var type = $('#type')
-          .val()
-          .trim();
-
-        var utilities = $('#utilities')
-          .val()
-          .trim();
-
-        var size = $('#size')
-          .val()
-          .trim();
-
-        database.ref().push({
-          address: address,
-          postalcode: postalcode,
-          price: price,
-          beds: beds,
-          baths: baths,
-          description: description,
-          type: type,
-          utilities: utilities,
-          size: size,
-          name: name,
-          email: email,
-          uid: uid,
-          dateAdded: firebase.database.ServerValue.TIMESTAMP
-        });
-
-        window.location.href = 'index.html';
-      }
-    });
+      });
+  });
 
   $('#cancel').on('click', function(event) {
     event.preventDefault();
