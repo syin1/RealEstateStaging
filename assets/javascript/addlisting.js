5713151000398c32f7f7bd149f291b945073db13
syin <shaomingyinut@gmail.com>
2018-07-31 19:06:17 -0400

    revert validation change

--- assets/javascript/addlisting.js
+++ assets/javascript/addlisting.js
@@ -15,82 +15,82 @@ $(document).ready(function() {
   $('#submit').on('click', function(event) {
     // event.preventDefault(); // this step is necessary to trigger the default validation
 
-    $('#addlistingform')
-      .validator()
-      .on('submit', function(e) {
-        if (e.isDefaultPrevented()) {
-          // handle the invalid form...
-          console.log(e.isDefaultPrevented());
-          console.log(e);
-        } else {
-          var user = firebase.auth().currentUser;
-          var name, email, photoUrl, uid, emailVerified;
+    // $('#addlistingform')
+    //   .validator()
+    //   .on('submit', function(e) {
+    //     if (e.isDefaultPrevented()) {
+    //       // handle the invalid form...
+    //       console.log(e.isDefaultPrevented());
+    //       console.log(e);
+    //     } else {
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
 
-          if (user != null) {
-            name = user.displayName;
-            email = user.email;
-            photoUrl = user.photoURL;
-            emailVerified = user.emailVerified;
-            uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-            // this value to authenticate with your backend server, if
-            // you have one. Use User.getToken() instead.
-          }
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
+    }
 
-          var address = $('#address')
-            .val()
-            .trim();
+    var address = $('#address')
+      .val()
+      .trim();
 
-          var postalcode = $('#postalcode')
-            .val()
-            .trim();
+    var postalcode = $('#postalcode')
+      .val()
+      .trim();
 
-          var price = $('#price')
-            .val()
-            .trim();
+    var price = $('#price')
+      .val()
+      .trim();
 
-          var beds = $('#beds')
-            .val()
-            .trim();
+    var beds = $('#beds')
+      .val()
+      .trim();
 
-          var baths = $('#baths')
-            .val()
-            .trim();
+    var baths = $('#baths')
+      .val()
+      .trim();
 
-          var description = $('#description')
-            .val()
-            .trim();
+    var description = $('#description')
+      .val()
+      .trim();
 
-          var type = $('#type')
-            .val()
-            .trim();
+    var type = $('#type')
+      .val()
+      .trim();
 
-          var utilities = $('#utilities')
-            .val()
-            .trim();
+    var utilities = $('#utilities')
+      .val()
+      .trim();
 
-          var size = $('#size')
-            .val()
-            .trim();
+    var size = $('#size')
+      .val()
+      .trim();
 
-          database.ref().push({
-            address: address,
-            postalcode: postalcode,
-            price: price,
-            beds: beds,
-            baths: baths,
-            description: description,
-            type: type,
-            utilities: utilities,
-            size: size,
-            name: name,
-            email: email,
-            uid: uid,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-          });
+    database.ref().push({
+      address: address,
+      postalcode: postalcode,
+      price: price,
+      beds: beds,
+      baths: baths,
+      description: description,
+      type: type,
+      utilities: utilities,
+      size: size,
+      name: name,
+      email: email,
+      uid: uid,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
-          window.location.href = 'index.html';
-        }
-      });
+    window.location.href = 'index.html';
+    //   }
+    // });
   });
 
   $('#cancel').on('click', function(event) {
