fcad8e202e5cbfa2cb2b28bd0dee9abf0ddb7806
syin <shaomingyinut@gmail.com>
2018-07-31 19:01:39 -0400

    adding external validation script for edit form

--- assets/javascript/editlisting.js
+++ assets/javascript/editlisting.js
@@ -25,86 +25,94 @@ $(document).ready(function() {
   $('#submit').on('click', function(event) {
     // event.preventDefault();
 
-    var user = firebase.auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
+    $('#editform')
+      .validator()
+      .on('submit', function(e) {
+        if (e.isDefaultPrevented()) {
+          // handle the invalid form...
+        } else {
+          var user = firebase.auth().currentUser;
+          var name, email, photoUrl, uid, emailVerified;
 
-    if (user != null) {
-      name = user.displayName;
-      email = user.email;
-      photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-      // this value to authenticate with your backend server, if
-      // you have one. Use User.getToken() instead.
-    }
+          if (user != null) {
+            name = user.displayName;
+            email = user.email;
+            photoUrl = user.photoURL;
+            emailVerified = user.emailVerified;
+            uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+            // this value to authenticate with your backend server, if
+            // you have one. Use User.getToken() instead.
+          }
 
-    var address = $('#address')
-      .val()
-      .trim();
+          var address = $('#address')
+            .val()
+            .trim();
 
-    var postalcode = $('#postalcode')
-      .val()
-      .trim();
+          var postalcode = $('#postalcode')
+            .val()
+            .trim();
 
-    var price = $('#price')
-      .val()
-      .trim();
+          var price = $('#price')
+            .val()
+            .trim();
 
-    var beds = $('#beds')
-      .val()
-      .trim();
+          var beds = $('#beds')
+            .val()
+            .trim();
 
-    var baths = $('#baths')
-      .val()
-      .trim();
+          var baths = $('#baths')
+            .val()
+            .trim();
 
-    var description = $('#description')
-      .val()
-      .trim();
+          var description = $('#description')
+            .val()
+            .trim();
 
-    var type = $('#type')
-      .val()
-      .trim();
+          var type = $('#type')
+            .val()
+            .trim();
 
-    var utilities = $('#utilities')
-      .val()
-      .trim();
+          var utilities = $('#utilities')
+            .val()
+            .trim();
 
-    var size = $('#size')
-      .val()
-      .trim();
+          var size = $('#size')
+            .val()
+            .trim();
 
-    database
-      .ref()
-      .child(sessionStorage.getItem('data-key'))
-      .set({
-        address: address,
-        postalcode: postalcode,
-        price: price,
-        beds: beds,
-        baths: baths,
-        description: description,
-        type: type,
-        utilities: utilities,
-        size: size,
-        name: name,
-        email: email,
-        uid: uid,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+          database
+            .ref()
+            .child(sessionStorage.getItem('data-key'))
+            .set({
+              address: address,
+              postalcode: postalcode,
+              price: price,
+              beds: beds,
+              baths: baths,
+              description: description,
+              type: type,
+              utilities: utilities,
+              size: size,
+              name: name,
+              email: email,
+              uid: uid,
+              dateAdded: firebase.database.ServerValue.TIMESTAMP
+            });
+
+          sessionStorage.setItem('address', address);
+          sessionStorage.setItem('baths', baths);
+          sessionStorage.setItem('beds', beds);
+          sessionStorage.setItem('description', description);
+          sessionStorage.setItem('postalcode', postalcode);
+          sessionStorage.setItem('price', price);
+          sessionStorage.setItem('size', size);
+          sessionStorage.setItem('type', type);
+          sessionStorage.setItem('utilities', utilities);
+          sessionStorage.setItem('email', email);
+
+          window.location.href = 'details.html';
+        }
       });
-
-    sessionStorage.setItem('address', address);
-    sessionStorage.setItem('baths', baths);
-    sessionStorage.setItem('beds', beds);
-    sessionStorage.setItem('description', description);
-    sessionStorage.setItem('postalcode', postalcode);
-    sessionStorage.setItem('price', price);
-    sessionStorage.setItem('size', size);
-    sessionStorage.setItem('type', type);
-    sessionStorage.setItem('utilities', utilities);
-    sessionStorage.setItem('email', email);
-
-    window.location.href = 'details.html';
   });
 
   $('#cancel').on('click', function(event) {
